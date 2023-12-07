d669824d1eb58a78d55fcf5f155be63e1409bec3
cole92 <aleksandar9212@gmail.com>
2023-12-07 09:00:51 +0100

    Implemented access to the necessary DOM elements for the quiz.
    Set up basic variables to track the quiz state (current question, answer acceptance, score, question counter).
    
    Initialized an empty array for quiz questions.
    
    Added a fetch request to retrieve questions from the Open Trivia Database API and map responses to the application's format.
    
    Started the process of processing and formatting questions for the quiz (work in progress).

---
+++ js/game.js
@@ -0,0 +1,28 @@
+// Accesing elements
+const question = document.querySelector('#question');
+const choices = Array.from(document.getElementsByClassName('choices-text'));
+const progressText = document.querySelector("#progressText");
+const scoresText = document.querySelector("#score");
+const progressBarFull = document.querySelector("#progressBarFull");
+const loader = document.querySelector("#loader");
+const game = document.querySelector('#game');
+// Initializing variables to track the quiz state
+let currentQuestions = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+// Empty array for quiz questions
+let questions = [];
+// Fetching quiz questions from the Open Trivia Database API
+fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
+.then(response => response.json())
+.then(loadedQuestionsObj => {
+    questions = loadedQuestionsObj.results.map(loadedQuestionElement => {
+        const formattedQuestion = {
+            question: loadedQuestionElement.question
+        }
+        const answerChoices = [...loadedQuestionElement.incorrect_answers];
+    })
+})
+
